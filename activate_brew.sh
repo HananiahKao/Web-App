752e21ff042e3e571b32f974cf82fb31bf11c757
H K <40303266-vs2hksbc7q@users.noreply.replit.com>
2025-03-23 08:31:21 +0000

    Add Homebrew activation script

---
+++ activate_brew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Activate Homebrew environment
+export PATH="$HOME/homebrew/bin:$PATH"
+echo "Homebrew has been activated. You can now use 'brew' commands."
+echo "Homebrew version: $(brew --version)"
